a688b651a22124652d22067ee2a21e1d572311d1
johnfinley <johnfinleybremerton@gmail.com>
2022-09-09 18:52:44 -0600

    .

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://jdh-authentication-app-react.herokuapp.com"
+    origins "https://phase-4-project.netlify.app"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
