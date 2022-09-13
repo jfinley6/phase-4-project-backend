acbf634cc3f15875c445c5bc9add6c5714e8bef2
+ <+>
2022-09-13 13:53:20 -0600

    Add Columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :posts, dependent: :destroy
+    has_many :comments
 
     validates_presence_of :email
     validates_uniqueness_of :email
