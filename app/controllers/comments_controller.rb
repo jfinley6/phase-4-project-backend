64c7302fdcf2c0fec21f15f674f2d1bc1878593b
+ <+>
2022-09-14 16:18:23 -0600

    Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,12 @@ class CommentsController < ApplicationController
         comments = Comment.all
         render json: comments, include: :post
       end
+      def create
+        comment = Comment.create!(
+            body: params['comment']['body'],
+            user_id: params['comment']['user_id'],
+            post_id: params['comment']['post_id']
+        )
+        render json: comment, status: :created
+    end
 end
