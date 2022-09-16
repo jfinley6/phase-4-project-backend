a4ca9bee0b9e8187bd7c0de006839f2af8a5888f
johnfinley <johnfinleybremerton@gmail.com>
2022-09-16 15:37:34 -0600

    .

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,14 @@ require_relative "boot"
 
 require "rails/all"
 
+config.load_defaults 6.1
+
+config.api_only = true
+
+config.middleware.use ActionDispatch::Cookies
+config.middleware.use ActionDispatch::Session::CookieStore
+config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "radiant-atoll-92288.herokuapp.com"
+if Rails.env.production?
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "radiant-atoll-92288.herokuapp.com", domain: :all, tld_length: 2
 else
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
