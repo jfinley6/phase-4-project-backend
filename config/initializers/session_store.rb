7944d916b64e416031563c0a28913197212c9d79
johnfinley <johnfinleybremerton@gmail.com>
2022-09-15 18:27:12 -0600

    .

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "phase-4-project.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://radiant-atoll-92288.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
