1e5f3af2afdb72c7a9a6e6732740ebad6801b962
johnfinley <johnfinleybremerton@gmail.com>
2022-09-17 13:44:32 -0600

    posts endpoint now includes comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
     def index
         posts = Post.all.order("created_at desc")
-        render json: posts, include: { user: { only: :username } }
+        render json: posts, include: {:comments => {:include => :user}, :user => {only: [:username]}}
     end
 
     def create
