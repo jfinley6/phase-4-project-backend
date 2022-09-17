437314e66930d5131ea7fc265f18ae1e57696db1
johnfinley <johnfinleybremerton@gmail.com>
2022-09-16 19:11:42 -0600

    .

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
-    include CurrentUserConcern
+    before_action :set_current_user
+
+    def set_current_user
+        if session[:user_id]
+            @current_user = User.find(session[:user_id])
+        end
+    end
 
     def create
         user = User.find_by(email: params["user"]["email"])
