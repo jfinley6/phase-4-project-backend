ae126285a54e9990f01d4b3f94f163e561c99458
johnfinley <johnfinleybremerton@gmail.com>
2022-09-16 19:07:34 -0600

    .

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params["user"]["email"])
-                   .try(:authenticate, params["user"]["password"])
+                   &.authenticate(params["user"]["password"])
 
         if user
             session[:user_id] = user.id
