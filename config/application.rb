25adb7bcd5a3898a4dd03d8c94af10d83219321b
johnfinley <johnfinleybremerton@gmail.com>
2022-09-16 19:01:49 -0600

    .

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,18 @@ Bundler.require(*Rails.groups)
 module AuthenticationApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.1
 
+    # This is set in apps generated with the --api flag, and removes session/cookie middleware
+    config.api_only = true
+
+    # ▾ Must add these lines! ▾
+    # Adding back cookies and session middleware
     config.middleware.use ActionDispatch::Cookies
-config.middleware.use ActionDispatch::Session::CookieStore
-config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    config.action_dispatch.cookies_same_site_protection = :strict
 
     # Configuration for the application, engines, and railties goes here.
     #
